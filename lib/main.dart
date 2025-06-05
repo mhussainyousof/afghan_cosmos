dd7365eb04a656b4cc21100a6128624cdc1fce94
Hussain Yousof <mhussainyousof@gmail.com>
2025-06-05 22:05:29 +0330

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:afghan_cosmos/firebase_options.dart';
 import 'package:afghan_cosmos/repo/auth_repo.dart';
 import 'package:afghan_cosmos/screens/home_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
