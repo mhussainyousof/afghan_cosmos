3979a50853e66ed78cf4b0891166a9c6e8e271ee
Hussain Yousof <mhussainyousof@gmail.com>
2025-06-05 22:05:16 +0330

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '/screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
